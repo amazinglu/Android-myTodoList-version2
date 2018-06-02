86e5f249b0db1818ef019bdec450b4da5bd17e4a
AmazingLu <amazinglu166@gmail.com>
2018-06-02 14:25:05 -0400

    todo_list_check_and_edit_complete

---
+++ app/src/main/java/com/example/amazinglu/todo_list/edit_list/EditListActivity.java
@@ -0,0 +1,17 @@
+package com.example.amazinglu.todo_list.all_list;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.amazinglu.todo_list.R;
+
+public class EditListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_list);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/amazinglu/todo_list/edit_list/EditListFragment.java
@@ -0,0 +1,6 @@
+package com.example.amazinglu.todo_list.edit_list;
+
+import android.support.v4.app.Fragment;
+
+public class editListFragment extends Fragment {
+}
